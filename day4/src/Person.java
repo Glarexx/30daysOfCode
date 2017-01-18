3c0093f88112113f8f5d2f8dad5faed53f8a4c72
Ievgenii Drozd <chap10@live.ru>
2017-01-18 11:09:02 -0500

    Day 5: Loops

--- day4/src/Person.java
+++ day4/src/Person.java
@@ -8,12 +8,17 @@ import java.util.*;
  */
 
 public class Person {
+    private static final String AGE_IS_NOT_VALID_SETTING_AGE_TO_0 = "Age is not valid, setting age to 0.";
+    private static final String YOU_ARE_YOUNG = "You are young.";
+    private static final String YOU_ARE_A_TEENAGER = "You are a teenager.";
+    private static final String YOU_ARE_OLD = "You are old.";
+
     private int age;
 
     private Person(int initialAge) {
         if (initialAge < 0) {
             this.age = 0;
-            System.out.println("Age is not valid, setting age to 0.");
+            System.out.println(AGE_IS_NOT_VALID_SETTING_AGE_TO_0);
         } else {
             this.age = initialAge;
         }
@@ -21,18 +26,18 @@ public class Person {
 
     private void amIOld() {
         if (this.age < 13) {
-            System.out.println("You are young.");
+            System.out.println(YOU_ARE_YOUNG);
         } else {
             if (this.age >= 13 && this.age < 18) {
-                System.out.println("You are a teenager.");
+                System.out.println(YOU_ARE_A_TEENAGER);
             } else {
-                System.out.println("You are old.");
+                System.out.println(YOU_ARE_OLD);
             }
         }
     }
 
     private void yearPasses() {
-        age++;
+        this.age++;
     }
 
     public static void main(String[] args) {
