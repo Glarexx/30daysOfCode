4e3321eaff5b5575b335e2608a4f9c3bb325fcb0
Ievgenii Drozd <chap10@live.ru>
2017-02-23 16:12:49 -0500

    Day 22: Binary Search Trees

---
+++ day23/src/Node.java
@@ -0,0 +1,14 @@
+/**
+ * @author Ievgenii Drozd
+ * @version 2/23/17
+ *          Day 22: Binary Search Trees
+ */
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
