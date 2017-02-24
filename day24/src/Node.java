0afe857f61e19b91434966909516e790ae54f2b2
Ievgenii Drozd <chap10@live.ru>
2017-02-24 09:39:40 -0500

    Day 23: BST Level-Order Traversal

---
+++ day24/src/Node.java
@@ -0,0 +1,15 @@
+/**
+ * @author Ievgenii Drozd
+ * @version 2/24/17
+ *          Day 24: More Linked Lists
+ */
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+
+}
