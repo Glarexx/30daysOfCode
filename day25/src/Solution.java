4c4ac4fe95ce75fab1d46cb09fd09e3b5cf4d9e6
Ievgenii Drozd <chap10@live.ru>
2017-02-25 11:01:19 -0500

    Day 25: Running Time and Complexity

---
+++ day25/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * @author Ievgenii Drozd
+ * @version 2/25/17
+ *          Day 25: Running Time and Complexity
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int number = sc.nextInt();
+            System.out.println(isPrime(number) ? "Prime" : "Not prime");
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) return false;
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
